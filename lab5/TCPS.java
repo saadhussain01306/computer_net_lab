c54f629bf08a8c5f5f8fdd3a3f961f07d4881d91
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-10-18 12:07:01 +0530

    Update TCPS.java

--- lab5/TCPS.java
+++ lab5/TCPS.java
@@ -1,3 +1,10 @@
+/* Using TCP/IP sockets, write a client – server program to make the 
+
+client send the file name and to make the server send back the contents 
+
+of the requested file if present*/
+
+
 /*SERVER program*/
 
 import java.net.*; 
