6110cde37fba4595cf2f47709e0d4edc060d7b44
Hisham Akmal <hisham0502@gmail.com>
2023-10-26 12:55:14 +0530

    Update UDPServer.java

--- lab6/UDPServer.java
+++ lab6/UDPServer.java
@@ -7,13 +7,15 @@ import java.net.*;
 
 public class UDPServer {
     public static void main(String args[]) throws Exception {
-        DatagramSocket serverSocket = new DatagramSocket(9884);
+    DatagramSocket serverSocket=null;
+    try{
+        serverSocket = new DatagramSocket(9877);
         System.out.println("Server is Ready for the client");
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
         
-         
             while (true) {
+            
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 serverSocket.receive(receivePacket);
                 String sentence = new String(receivePacket.getData());
@@ -24,9 +26,14 @@ public class UDPServer {
                 sendData = capitalizedSentence.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
                 serverSocket.send(sendPacket);
-            }
+            }           
+    }catch (Exception e) {
+                    e.printStackTrace();
+                }finally{
+                serverSocket.close();              
          
-    }
+      }
+  }
 }
 
 /*first run the server program and then the client program in another terminal*/
