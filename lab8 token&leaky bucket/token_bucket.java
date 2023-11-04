eb82752d9ee7a3a01648cd611fa7980ceb07b945
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-11-04 19:23:11 +0530

    Update token_bucket.java

--- lab8 token&leaky bucket/token_bucket.java
+++ lab8 token&leaky bucket/token_bucket.java
@@ -28,36 +28,6 @@ public class Token_bucket{
                 token_remaining+=token_requested;
             }
             System.out.println(String.format("%d\t%d\t%d\t%d", i + 1, token_requested, token_sent, token_remaining));
-        }/*Token Bucket for congesstion control*/
-
-/* In this the bucket is filled with constant rate called token_generation_rate untill the bucket is full */
-
-import java.util.Scanner;
-import java.util.*;
-
-public class Token_bucket{
-    public static void main(String args[]){
-        Scanner in = new Scanner(System.in);
-        int token_remaining=0;//total tokens in the bucket
-        int token_requested,token_sent;
-        System.out.println("Enter the bucket capacity");
-        int bucket_capacity=in.nextInt();
-        System.out.println("Enter the Token generation rate (Rate at which tokens are sent to the bucket)");
-        int token_gen_rate=in.nextInt();
-        System.out.println("Enter the number of Cycles the host computer sends the Tokens to the bucket(at constant rate)");
-        int n=in.nextInt();
-        System.out.println(String.format("%s\t%s\t%s\t%s", "TimeΔt", "Tokens Requested", "Tokens Sent", "Tokens Remaining in bucket"));
-        for(int i=0;i<n;i++){
-            token_requested=token_gen_rate;
-            if(token_requested+token_remaining>bucket_capacity){
-                token_sent=bucket_capacity-token_remaining;
-                token_remaining=bucket_capacity;
-            }
-            else{
-                token_sent=token_requested;
-                token_remaining+=token_requested;
-            }
-            System.out.println(String.format("%d\t%d\t%d\t%d", i + 1, token_requested, token_sent, token_remaining));
         }
     }
 }
