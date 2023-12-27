190c24968da81827501073f8b927908dc514f70d
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-12-28 01:52:59 +0530

    Update UDPClient.java

--- lab6/UDPClient.java
+++ lab6/UDPClient.java
@@ -1,30 +1,29 @@
 /*UDP cleint program*/
 
-import java.io.*; 
-import java.net.*;
-     public class UDPClient
-         {
-           public static void main(String[] args) throws Exception
-         {
-          BufferedReader inFromUser=new BufferedReader(new InputStreamReader(System.in));
-          DatagramSocket clientSocket=new DatagramSocket();
-          InetAddress IPAddress=InetAddress.getByName("localhost");
-          byte[] sendData = new byte[1024];
-          byte[] receiveData=new byte[1024];
-          System.out.println("Enter the string in lowercase so that you receive the message in Uppercase from the server");
-          String sentence=inFromUser.readLine();
-          sendData=sentence.getBytes();
-         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9884);
-          clientSocket.send(sendPacket);
-          DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
-          clientSocket.receive(receivePacket);
-          String modifiedSentence=new String(receivePacket.getData());
-          System.out.println("FROM SERVER: "+modifiedSentence);
-          clientSocket.close();
-               
-          }
-}
+import java.io.*;
+import java.net.*; 
+class UDPClient
+public static void main(String[] args) throws Exception
+{
+BufferedReader inFromUser=new BufferedReader(new InputStreamReader(System.in));
+DatagramSocket clientSocket-new DatagramSocket();
+InetAddress IPAddress=InetAddress.getByName("localhost");
+byte[] sendData=new byte[1024];
+byte[] receiveData=new byte[1024];
+System.out.println("Enter the sting to be converted in to Upper case");
+String sentence=inFromUser.readLine();
+sendData=sentence.getBytes();
+DatagramPacket sendPacket=new
+DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+clientSocket.send(sendPacket);
+DatagramPacket receivePacket=new DatagramPacket(receiveData, receiveData.length);
+clientSocket.receive(receivePacket);
+String modified Sentence=new String(receivePacket.getData());
+System.out.println("FROM SERVER: "+modifiedSentence);
 
+ clientSocket.close();
+}
+     
 /*output;-
   first run the server program in one terminal in another terminal run the client program
 Server side
