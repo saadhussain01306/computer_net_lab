b9c94fa75f2592f66ce07a125aa1eaa2de422092
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-12-28 01:36:38 +0530

    Update TCPC.java

--- lab5/TCPC.java
+++ lab5/TCPC.java
@@ -1,30 +1,33 @@
 /*Client program*/
 
 import java.net.*;
-import java.io.*; 
-  public class TCPC
-    {
-       public static void main(String[] args) throws Exception
-       {
-         Socket sock=new Socket("127.0.01", 4000);
-          System.out.println("Enter the filename");
-         BufferedReader keyRead=new BufferedReader(new InputStreamReader(System.in));
-         String fname=keyRead.readLine();
-         OutputStream ostream=sock.getOutputStream();
-         PrintWriter pwrite=new PrintWriter(ostream, true);
-         pwrite.println(fname);
-         InputStream istream=sock.getInputStream();
-         BufferedReader socketRead=new BufferedReader(new InputStreamReader(istream));
-         String str;
-         while((str=socketRead.readLine())!=null)
-           {
-              System.out.println(str);
-           }
-         pwrite.close();
-         socketRead.close();
-         keyRead.close();
-       }
-    }       
+import java.io.*;
+
+public class TCPC {
+    public static void main(String[] args) throws Exception {
+        Socket sock = new Socket("127.0.0.1", 4000);
+        System.out.println("Enter the filename");
+        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+        String fname = keyRead.readLine();
+
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+        pwrite.println(fname);
+
+        InputStream istream = sock.getInputStream();
+        BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
+
+        String str;
+        while ((str = socketRead.readLine()) != null) {
+            System.out.println(str);
+        }
+
+        pwrite.close();
+        socketRead.close();
+        keyRead.close();
+    }
+}
+
        
 /*First run the server program then in new terminal window run the client program and input the 
 file name the server will return the file content if it exists
